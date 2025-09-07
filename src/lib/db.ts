213c6b40e45242dabe6404d64a8a024c8a71f790
MaciejkaG <me@mcjk.cc>
2025-09-07 13:44:34 +0200

    add pnpm files

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,7 +5,7 @@ const client = createClient({
   url: process.env.REDIS_URL ?? "redis://localhost:6379",
 });
 
-client.on("error", (err) => console.log("Redis Client Error", err));
+client.on("error", (err: unknown) => console.log("Redis Client Error", err));
 
 await client.connect();
 
