05d72a9dadb7e5919e31bb46569e5fc6af1972fe
Maciej Gomoła <me@mcjk.cc>
2026-05-13 22:47:43 +0200

    fix: force dynamic root page for turnstile sitekey

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { HomeClient } from "@/components/HomeClient";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return <HomeClient sitekey={process.env.TURNSTILE_SITE_KEY!} />;
 }
